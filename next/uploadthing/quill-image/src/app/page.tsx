a34e6b4fd60426315160dc56d27450627f9a7083
Cono <everydaycono@gmail.com>
2023-09-13 16:51:44 +0900

    uploadthing: quill-editor init

--- next/uploadthing/quill-image/src/app/page.tsx
+++ next/uploadthing/quill-image/src/app/page.tsx
@@ -1,7 +1,9 @@
-import Image from 'next/image';
+import QuillWrapper, { formats, modules } from '@/components/RichEditor';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <QuillWrapper modules={modules} formats={formats} />
+    </main>
   );
 }
---
+++ next/uploadthing/quill-image/src/components/RichEditor.tsx
@@ -0,0 +1,54 @@
+'use client';
+
+import dynamic from 'next/dynamic';
+import 'react-quill/dist/quill.snow.css';
+import type ReactQuill from 'react-quill';
+const QuillWrapper = dynamic(
+  async () => {
+    const { default: RQ } = await import('react-quill');
+    // eslint-disable-next-line react/display-name
+    return ({ ...props }) => <RQ {...props} />;
+  },
+  {
+    ssr: false,
+  }
+) as typeof ReactQuill;
+
+export default QuillWrapper;
+
+export const modules = {
+  toolbar: [
+    [{ header: '1' }, { header: '2' }, { font: [] }],
+    [{ size: [] }],
+    ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+    [
+      { list: 'ordered' },
+      { list: 'bullet' },
+      { indent: '-1' },
+      { indent: '+1' },
+    ],
+    ['link', 'image', 'video'],
+    ['clean'],
+  ],
+  clipboard: {
+    // toggle to add extra line breaks when pasting HTML:
+    matchVisual: false,
+  },
+};
+
+export const formats = [
+  'header',
+  'font',
+  'size',
+  'bold',
+  'italic',
+  'underline',
+  'strike',
+  'blockquote',
+  'list',
+  'bullet',
+  'indent',
+  'link',
+  'image',
+  'video',
+];
