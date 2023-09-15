2b10a3250a7d9d2504bcd33bdc8b6d355e8fb639
Cono <everydaycono@gmail.com>
2023-09-16 01:25:56 +0900

    test: test quill

--- next/uploadthing/quill-image/src/app/page.tsx
+++ next/uploadthing/quill-image/src/app/page.tsx
@@ -1,11 +1,16 @@
-import RichEditor from '@/components/RichEditor';
+// import RichEditor from '@/components/RichEditor';
 // import { UploadButton } from '@/utils/uploadthing';
 // You need to import our styles for the button to look right. Best to import in the root /layout.tsx but this is fine
+// import EditorComponent from '@/components/Test4/Test4';
+
+import { Test2 } from '@/components/Test2';
 import '@uploadthing/react/styles.css';
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <RichEditor />
+      {/* <RichEditor /> */}
+      {/* <EditorComponent /> */}
+      <Test2 />
     </main>
   );
 }
